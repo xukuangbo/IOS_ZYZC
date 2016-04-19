a068d2aa5c9f3a44aa4e6b8f423c0575bc0133b0
liuliang <liuliang@liuliang.local>
2016-04-19 19:17:37 +0800

    no message

---
+++ ios_zhongyouzhongchou/ios_zhongyouzhongchou/classes/ZYZCRaise(众筹)/ZCView/ZCDetailFirstCell.m
@@ -0,0 +1,21 @@
+//
+//  ZCDetailFirstCell.m
+//  ios_zhongyouzhongchou
+//
+//  Created by liuliang on 16/4/19.
+//  Copyright © 2016年 liuliang. All rights reserved.
+//
+
+#import "ZCDetailFirstCell.h"
+
+@implementation ZCDetailFirstCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ ios_zhongyouzhongchou/ios_zhongyouzhongchou/classes/ZYZCRaise(众筹)/ZCView/ZCDetailSecondCell.m
@@ -0,0 +1,23 @@
+//
+//  ZCDetailSecondCell.m
+//  ios_zhongyouzhongchou
+//
+//  Created by liuliang on 16/4/19.
+//  Copyright © 2016年 liuliang. All rights reserved.
+//
+
+#import "ZCDetailSecondCell.h"
+
+@implementation ZCDetailSecondCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
