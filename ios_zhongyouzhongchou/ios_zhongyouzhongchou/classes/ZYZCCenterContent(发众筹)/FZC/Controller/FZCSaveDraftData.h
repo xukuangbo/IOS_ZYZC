e714af1d746f76624a8b5d37abb01e7b8a0e3572
liuliang <liuliang@liuliang.local>
2016-05-25 18:00:47 +0800

    no message

---
+++ ios_zhongyouzhongchou/ios_zhongyouzhongchou/classes/ZYZCCenterContent(发众筹)/FZC/Controller/FZCSaveDraftData.h
@@ -0,0 +1,13 @@
+//
+//  FZCSaveDraftData.h
+//  ios_zhongyouzhongchou
+//
+//  Created by liuliang on 16/5/25.
+//  Copyright © 2016年 liuliang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FZCSaveDraftData : NSObject
+
+@end
---
+++ ios_zhongyouzhongchou/ios_zhongyouzhongchou/classes/ZYZCCenterContent(发众筹)/FZC/Controller/FZCSaveDraftData.m
@@ -0,0 +1,19 @@
+//
+//  FZCSaveDraftData.m
+//  ios_zhongyouzhongchou
+//
+//  Created by liuliang on 16/5/25.
+//  Copyright © 2016年 liuliang. All rights reserved.
+//
+
+#import "FZCSaveDraftData.h"
+#import "MoreFZCDataManager.h"
+
+@implementation FZCSaveDraftData
+
++ (void)saveDraftDataIn
+{
+    
+}
+
+@end
