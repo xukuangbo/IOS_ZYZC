38a6b4202042f4ddd60f5dad62c0e3692dfc75b7
mac <mac@macdeMacBook-Pro.local>
2016-05-31 16:48:35 +0800

    no message

--- ios_zhongyouzhongchou/ios_zhongyouzhongchou/classes/ZYZCMine(我的)/MineSetUp(个人设置)/PersonSetUpVC(个人设置控制器)/Controller/MinePersonSetUpLikeController.m
+++ ios_zhongyouzhongchou/ios_zhongyouzhongchou/classes/ZYZCMine(我的)/MineSetUp(个人设置)/PersonSetUpVC(个人设置控制器)/Controller/MinePersonSetUpLikeController.m
@@ -31,43 +31,43 @@
     
     self.title = @"选择旅行标签";
     
-    [self createUI];
+//    [self createUI];
 }
 
-- (void)createUI
-{
-    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-    [self.view addSubview:scrollView];
-    self.scrollView = scrollView;
-    scrollView.backgroundColor = [UIColor redColor];
-    
-    //白色背景容器
-    CGFloat mapViewX = KEDGE_DISTANCE;
-    CGFloat mapViewY = KEDGE_DISTANCE;
-    CGFloat mapViewW = KSCREEN_W - KEDGE_DISTANCE * 2;
-    CGFloat mapViewH = 300;
-    UIImageView *mapView = [[UIImageView alloc] initWithFrame:CGRectMake(mapViewX, mapViewY, mapViewW, mapViewH)];
-    mapView.layer.cornerRadius = 5;
-    mapView.layer.masksToBounds = YES;
-    mapView.image = KPULLIMG(@"tab_bg_boss0", 5, 0, 5, 0);
-    [scrollView addSubview:mapView];
-    
-    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
-    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(10, 10, mapView.width - 10 * 2, mapView.height - 10 * 2) collectionViewLayout:flowLayout];
-    collectionView registerClass:[] forCellWithReuseIdentifier:<#(nonnull NSString *)#>
-    collectionView.delegate = self;
-    collectionView.dataSource = self;
-    [scrollView addSubview:collectionView];
-    
-}
-
-#pragma mark - UICollectionViewDataSource
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
-{
-    return 20;
-}
-- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
-{
-    UICollectionViewCell *cell =
-}
+//- (void)createUI
+//{
+//    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+//    [self.view addSubview:scrollView];
+//    self.scrollView = scrollView;
+//    scrollView.backgroundColor = [UIColor redColor];
+//    
+//    //白色背景容器
+//    CGFloat mapViewX = KEDGE_DISTANCE;
+//    CGFloat mapViewY = KEDGE_DISTANCE;
+//    CGFloat mapViewW = KSCREEN_W - KEDGE_DISTANCE * 2;
+//    CGFloat mapViewH = 300;
+//    UIImageView *mapView = [[UIImageView alloc] initWithFrame:CGRectMake(mapViewX, mapViewY, mapViewW, mapViewH)];
+//    mapView.layer.cornerRadius = 5;
+//    mapView.layer.masksToBounds = YES;
+//    mapView.image = KPULLIMG(@"tab_bg_boss0", 5, 0, 5, 0);
+//    [scrollView addSubview:mapView];
+//    
+//    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+//    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(10, 10, mapView.width - 10 * 2, mapView.height - 10 * 2) collectionViewLayout:flowLayout];
+////    collectionView registerClass:[] forCellWithReuseIdentifier:<#(nonnull NSString *)#>
+//    collectionView.delegate = self;
+//    collectionView.dataSource = self;
+//    [scrollView addSubview:collectionView];
+//    
+//}
+//
+//#pragma mark - UICollectionViewDataSource
+//- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+//{
+//    return 20;
+//}
+//- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    UICollectionViewCell *cell =
+//}
 @end
