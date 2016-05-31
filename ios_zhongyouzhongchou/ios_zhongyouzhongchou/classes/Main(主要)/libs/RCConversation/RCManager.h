86f6729cb38e5e28b53007cce73728f190a18793
liuliang <liuliang@liuliang.local>
2016-05-31 16:41:41 +0800

    no message

---
+++ ios_zhongyouzhongchou/ios_zhongyouzhongchou/classes/Main(主要)/libs/RCConversation/RCManager.h
@@ -0,0 +1,13 @@
+//
+//  RCManager.h
+//  ios_zhongyouzhongchou
+//
+//  Created by liuliang on 16/5/31.
+//  Copyright © 2016年 liuliang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCManager : NSObject
+
+@end
---
+++ ios_zhongyouzhongchou/ios_zhongyouzhongchou/classes/Main(主要)/libs/RCConversation/ZYZCRCManager.m
@@ -0,0 +1,13 @@
+//
+//  RCManager.m
+//  ios_zhongyouzhongchou
+//
+//  Created by liuliang on 16/5/31.
+//  Copyright © 2016年 liuliang. All rights reserved.
+//
+
+#import "RCManager.h"
+
+@implementation RCManager
+
+@end
